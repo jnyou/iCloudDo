7867ed14f14de3f0a896d90fb01b4de685edd197
jnyou <1447177020@qq.com>
2020-11-26 11:45:38 +0800

    solve cloud oss Not compatible question

--- project-cluster-service/gmall/gmall-rear-server/product-service/src/test/java/org/jnyou/gmall/productservice/ProductServiceApplicationTests.java
+++ project-cluster-service/gmall/gmall-rear-server/product-service/src/test/java/org/jnyou/gmall/productservice/ProductServiceApplicationTests.java
@@ -44,17 +44,14 @@ class ProductServiceApplicationTests {
         // 引入阿里云oss组件存储：aliyun-oss-spring-boot-starter
         String bucketName = "smallsword-mall";
 // <yourObjectName>上传文件到OSS时需要指定包含文件后缀在内的完整路径，例如abc/efg/123.jpg。
-        String objectName = "37087592-8715-455c-AF06-F487E3731003.png";
+        String objectName = "liunian.jpg";
 
 // 创建OSSClient实例。
 //        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
 
 // 上传文件到指定的存储空间（bucketName）并将其保存为指定的文件名称（objectName）。
-        InputStream inputStream = new FileInputStream("C:\\Users\\YJN\\Pictures\\Saved Pictures\\37087592-8715-455c-AF06-F487E3731003.png");
+        InputStream inputStream = new FileInputStream("C:\\Users\\cizing\\Pictures\\liunian.jpg");
         ossClient.putObject(bucketName, objectName, inputStream);
-
-// 关闭OSSClient。
-        ossClient.shutdown();
     }
 
 }
