23a90a130c8327dbc30dfaf0e2d33c4b687adaa0
jnyou <1447177020@qq.com>
2020-11-30 17:48:10 +0800

    仓库维护 interface complete

--- project-cluster-service/gmall/gmall-rear-server/storage-service/src/main/java/org/jnyou/gmall/storageservice/service/impl/WareInfoServiceImpl.java
+++ project-cluster-service/gmall/gmall-rear-server/storage-service/src/main/java/org/jnyou/gmall/storageservice/service/impl/WareInfoServiceImpl.java
@@ -1,16 +1,18 @@
 package org.jnyou.gmall.storageservice.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.jnyou.common.utils.PageUtils;
 import org.jnyou.common.utils.Query;
-
 import org.jnyou.gmall.storageservice.dao.WareInfoDao;
 import org.jnyou.gmall.storageservice.entity.WareInfoEntity;
 import org.jnyou.gmall.storageservice.service.WareInfoService;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.Map;
 
 
 @Service("wareInfoService")
@@ -18,9 +20,21 @@ public class WareInfoServiceImpl extends ServiceImpl<WareInfoDao, WareInfoEntity
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        LambdaQueryWrapper<WareInfoEntity> lambdaQueryWrapper = Wrappers.<WareInfoEntity>lambdaQuery();
+
+        if (!StringUtils.isEmpty(params.get("key"))) {
+            lambdaQueryWrapper.eq(WareInfoEntity::getId, params.get("key"))
+                    .or()
+                    .like(WareInfoEntity::getName, params.get("key"))
+                    .or()
+                    .like(WareInfoEntity::getAddress, params.get("key"))
+                    .or()
+                    .like(WareInfoEntity::getAreacode, params.get("key"));
+        }
+
         IPage<WareInfoEntity> page = this.page(
                 new Query<WareInfoEntity>().getPage(params),
-                new QueryWrapper<WareInfoEntity>()
+                lambdaQueryWrapper
         );
 
         return new PageUtils(page);
