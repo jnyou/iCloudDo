3cab914c98dd8a8b28cf1ec75c6bc1befaf15cff
jnyou <1447177020@qq.com>
2020-12-13 00:33:37 +0800

    优化项目响应时间，使用jvisual vm 监控jvm内存。使用nginx动静分离

--- project-cluster-service/gmall/gmall-rear-server/product-service/src/main/java/org/jnyou/gmall/productservice/web/IndexController.java
+++ project-cluster-service/gmall/gmall-rear-server/product-service/src/main/java/org/jnyou/gmall/productservice/web/IndexController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -39,6 +40,12 @@ public class IndexController {
     public Map<String, List<Catelog2Vo>> getCatelogJson(){
         Map<String, List<Catelog2Vo>> objectMap = categoryService.getCatelogJson();
         return objectMap;
+    }
+
+    @ResponseBody
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
     }
 
 }
