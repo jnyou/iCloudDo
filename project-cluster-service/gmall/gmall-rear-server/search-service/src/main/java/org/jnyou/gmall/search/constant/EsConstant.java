813352f4bdbd7f921f536043df0ad4259c485496
jnyou <1447177020@qq.com>
2020-12-23 20:05:03 +0800

    show es return search page info

--- project-cluster-service/gmall/gmall-rear-server/search-service/src/main/java/org/jnyou/gmall/search/constant/EsConstant.java
+++ project-cluster-service/gmall/gmall-rear-server/search-service/src/main/java/org/jnyou/gmall/search/constant/EsConstant.java
@@ -9,6 +9,6 @@ package org.jnyou.gmall.search.constant;
 public class EsConstant {
 
     public static final String PRODUCT_INDEX = "gm_product";
-    public static final Integer PRODUCT_PAGESIZE = 2;
+    public static final Integer PRODUCT_PAGESIZE = 8;
 
 }
