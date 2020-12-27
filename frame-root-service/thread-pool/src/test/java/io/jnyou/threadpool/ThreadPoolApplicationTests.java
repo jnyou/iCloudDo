198a875d59cda662fa7bb22c3dd190fd85b86b6b
jnyou <1447177020@qq.com>
2020-12-27 14:17:39 +0800

    创建线程module

---
+++ frame-root-service/thread-pool/src/test/java/io/jnyou/threadpool/ThreadPoolApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jnyou.threadpool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadPoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
