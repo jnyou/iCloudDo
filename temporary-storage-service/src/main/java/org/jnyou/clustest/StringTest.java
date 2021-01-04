557caccf3a646ce71f4f45f35bcf7b3171cab52a
jnyou <1447177020@qq.com>
2021-01-04 19:38:17 +0800

    integrated xxl-job module

---
+++ temporary-storage-service/src/main/java/org/jnyou/clustest/StringTest.java
@@ -0,0 +1,18 @@
+package org.jnyou.clustest;
+
+/**
+ * <p>
+ *
+ * @className StringTest
+ * @author: JnYou xiaojian19970910@gmail.com
+ **/
+public class StringTest {
+    public static void main(String[] args) {
+
+        String [] str = {"aaa","bbb","ccc","eee","fff","ggg"};
+
+        System.out.println(str[str.length-2]);
+
+    }
+
+}
