ad0faad6963beed759874a3adc686defe27b8ba2
jnyou <1447177020@qq.com>
2021-01-09 19:01:38 +0800

    gain quartz module To be tested

---
+++ frame-root-service/quartz/src/test/java/io/jnyou/quartz/QuartzApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jnyou.quartz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
