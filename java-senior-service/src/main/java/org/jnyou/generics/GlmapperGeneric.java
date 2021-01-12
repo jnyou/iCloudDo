4dd9a7f706d72e7ff69231fb4d9f65655a3e14e1
jnyou <1447177020@qq.com>
2021-01-12 15:59:36 +0800

    learn generic note+

--- java-senior-service/src/main/java/org/jnyou/generics/GlmapperGeneric.java
+++ java-senior-service/src/main/java/org/jnyou/generics/GlmapperGeneric.java
@@ -1,5 +1,6 @@
 package org.jnyou.generics;
 
+import lombok.EqualsAndHashCode;
 import org.jnyou.generics.entity.Animal;
 
 import java.util.ArrayList;
@@ -21,7 +22,7 @@ public class GlmapperGeneric<T> {
      */
     private T t;
 
-    public void setT(T t){
+    public void setT(T t) {
         this.t = t;
     }
 
@@ -44,7 +45,7 @@ public class GlmapperGeneric<T> {
         String str = glmapperGeneric.getT();
         System.out.println(str);
     }
-    
+
     /**
      * ？表示不确定的 java 类型
      * T (type) 表示具体的一个java类型
@@ -52,19 +53,17 @@ public class GlmapperGeneric<T> {
      * E (element) 代表Element
      */
 
-    static int countLegs(List<? extends Animal> animals){
+    static int countLegs(List<? extends Animal> animals) {
         int retVal = 0;
-        for ( Animal animal : animals )
-        {
+        for (Animal animal : animals) {
             retVal += animal.getLegs();
         }
         return retVal;
     }
 
-    static int countLegs1 (List<Animal> animals ){
+    static int countLegs1(List<Animal> animals) {
         int retVal = 0;
-        for ( Animal animal : animals )
-        {
+        for (Animal animal : animals) {
             retVal += animal.getLegs();
         }
         return retVal;
@@ -72,11 +71,36 @@ public class GlmapperGeneric<T> {
 
     public static void main(String[] args) {
         List<Animal.Dog> dogs = new ArrayList<>();
-        // 不会报错
+        // 不会报错 List<? extends Animal> ： 表示当前可以传入Animal类或者它的子类，上界通配符
         countLegs(dogs);
         // 报错
 //        countLegs1(dogs);
+
+        List<Animal.Dog> dogs1 = new ArrayList<>();
+        List<Animal> animals = new ArrayList<>();
+        test(animals,dogs1);
     }
 
+    // 上界通配符 < ? extends E> : 表示这个泛型中的参数必须是 E 或者 E 的子类
+    private <K extends A, E extends B> E test(K arg1, E arg2) {
+        E result = arg2;
+        return result;
+    }
+
+
+    private class A {
+    }
+
+    private class B {
+    }
+
+    // 下界通配符 < ? super E> : 表示这个泛型中的参数必须是 E 或者 E 的父类
+    private static  <T> void test (List<? super T> dst , List<T> src) {
+        for (T t1 : src) {
+            dst.add(t1);
+        }
+    }
+
+    // Class<T> 和 Class<?> 区别
 
 }
