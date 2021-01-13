86c0b8f876ffca6955abe3acc86918cc2f3c82e6
jnyou <1447177020@qq.com>
2021-01-13 18:13:55 +0800

    login init

---
+++ project-cluster-service/cloud-rear-server/auth-service/src/main/java/org/jnyou/mall/auth/controller/LoginController.java
@@ -0,0 +1,29 @@
+package org.jnyou.mall.auth.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 代码千万行，注释第一行
+ * 注释不规范，同事泪两行
+ * <p>
+ * LoginController
+ *
+ * @version 1.0.0
+ * @author: JnYou
+ **/
+@Controller
+public class LoginController {
+
+    @GetMapping("login.html")
+    public String toLogin(){
+        return "login";
+    }
+
+    @GetMapping("reg.html")
+    public String toReg(){
+        return "reg";
+    }
+
+}
