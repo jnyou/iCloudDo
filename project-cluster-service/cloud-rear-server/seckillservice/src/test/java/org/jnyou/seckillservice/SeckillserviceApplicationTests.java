12e8b11bffcf5efbc1a49fc8161b1b446ea47d84
jnyou <1447177020@qq.com>
2021-02-16 20:45:42 +0800

    新增秒杀服务

---
+++ project-cluster-service/cloud-rear-server/seckillservice/src/test/java/org/jnyou/seckillservice/SeckillserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jnyou.seckillservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeckillserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
