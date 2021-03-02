f7decf3d97727e813374cf44eebc1dece3828bcb
jnyou <1447177020@qq.com>
2021-03-02 15:09:38 +0800

    定义切面日志类

---
+++ project-cluster-service/coin-exchange/coin-common/src/main/java/io/jnyou/aspect/WebLogAspect.java
@@ -0,0 +1,109 @@
+package io.jnyou.aspect;
+
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.core.util.URLUtil;
+import com.alibaba.fastjson.JSON;
+import io.jnyou.model.WebLog;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.LocalVariableTableParameterNameDiscoverer;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.HashMap;
+
+@Aspect
+@Component
+@Order(1)
+@Slf4j
+public class WebLogAspect {
+
+    /**
+     * 日志记录，logstash处理
+     */
+
+    @Pointcut("execution( * io.jnyou.controller.*.*(..))")
+    public void webLog() {
+    }
+
+    @Around(value = "webLog()")
+    public Object recordWebLog(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        Object result = null;
+        // 创建计时器
+        StopWatch stopWatch = new StopWatch();
+        //  开始计时器
+        stopWatch.start();
+        // 不需要我们自己处理这个异常
+        result = proceedingJoinPoint.proceed(proceedingJoinPoint.getArgs());
+        // 记时结束
+        stopWatch.stop();
+
+        // 获取请求的上下文
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = requestAttributes.getRequest();
+        // 获取登录的用户
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        // 获取方法
+        MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+        Method method = methodSignature.getMethod();
+        // 获取方法上的ApiOperation注解
+        ApiOperation annotation = method.getAnnotation(ApiOperation.class);
+        // 获取目标对象的类型名称
+        String className = proceedingJoinPoint.getTarget().getClass().getName();
+        // 获取请求的url 地址
+        String requestUrl = request.getRequestURL().toString();
+        WebLog webLog = WebLog.builder()
+                .basePath(StrUtil.removeSuffix(requestUrl, URLUtil.url(requestUrl).getPath())) // http://ip:port/
+                .description(annotation == null ? "no desc" : annotation.value())
+                .ip(request.getRemoteAddr())
+                .parameter(getMethodParameter(method, proceedingJoinPoint.getArgs()))
+                .method(className + "." + method.getName())
+                .result(request == null ? "" : JSON.toJSONString(request))
+                .recodeTime(System.currentTimeMillis())
+                .spendTime(stopWatch.getTotalTimeMillis())
+                .uri(request.getRequestURI())
+                .url(request.getRequestURL().toString())
+                .username(authentication == null ? "anonymous" : authentication.getPrincipal().toString())
+                .build();
+        log.info(JSON.toJSONString(webLog, true));
+        return result;
+    }
+
+    /**
+     * {
+     * "":value,
+     * "":"value"
+     * }
+     * 获取方法执行的参数
+     *
+     * @param method
+     * @param args
+     * @return
+     */
+    private Object getMethodParameter(Method method, Object[] args) {
+        LocalVariableTableParameterNameDiscoverer localVariableTableParameterNameDiscoverer = new LocalVariableTableParameterNameDiscoverer();
+        // 方法的形参名称
+        String[] parameterNames = localVariableTableParameterNameDiscoverer.getParameterNames(method);
+        HashMap<String, Object> methodParameters = new HashMap<>(124);
+        Parameter[] parameters = method.getParameters();
+        if (args != null) {
+            for (int i = 0; i < parameterNames.length; i++) {
+                methodParameters.put(parameterNames[i], args[i] == null ? "" : JSON.toJSONString(args[i]));
+            }
+        }
+        return methodParameters;
+    }
+}
---
+++ project-cluster-service/coin-exchange/coin-common/src/main/java/io/jnyou/model/WebLog.java
@@ -0,0 +1,70 @@
+package io.jnyou.model;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * web 操作日志记录
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Builder
+public class WebLog {
+    /**
+     * 操作描述
+     */
+    private String description;
+
+    /**
+     * 操作用户
+     */
+    private String username;
+
+    /**
+     * 当前时间
+     */
+    private Long recodeTime;
+
+    /**
+     * 消耗时间
+     */
+    private Long spendTime;
+
+    /**
+     * 根路径
+     */
+    private String basePath;
+
+    /**
+     * URI
+     */
+    private String uri;
+
+    /**
+     * URL
+     */
+    private String url;
+
+    /**
+     * 请求类型
+     */
+    private String method;
+
+    /**
+     * IP地址
+     */
+    private String ip;
+
+    /**
+     * 请求参数
+     */
+    private Object parameter;
+
+    /**
+     * 返回结果
+     */
+    private Object result;
+
+
+}
