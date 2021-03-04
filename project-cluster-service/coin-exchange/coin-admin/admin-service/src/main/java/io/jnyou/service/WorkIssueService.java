2f60c3cdf6b04c8fe79171df9d2b5549715e48f9
jnyou <1447177020@qq.com>
2021-03-04 16:53:52 +0800

    新增客服工单

--- project-cluster-service/coin-exchange/coin-admin/admin-service/src/main/java/io/jnyou/service/WorkIssueService.java
+++ project-cluster-service/coin-exchange/coin-admin/admin-service/src/main/java/io/jnyou/service/WorkIssueService.java
@@ -1,8 +1,20 @@
 package io.jnyou.service;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.jnyou.domain.WorkIssue;
 import com.baomidou.mybatisplus.extension.service.IService;
 public interface WorkIssueService extends IService<WorkIssue>{
-
-
+    /**
+     * 条件分页查询工单列表
+     * @param page
+     *  分页参数
+     * @param status
+     * 工单的状态
+     * @param startTime
+     * 查询的工单创建起始时间
+     * @param endTime
+     * 查询的工单创建截至时间
+     * @return
+     */
+    Page<WorkIssue> findByPage(Page<WorkIssue> page, Integer status, String startTime, String endTime);
 }
--- project-cluster-service/coin-exchange/coin-admin/admin-service/src/main/java/io/jnyou/service/impl/WorkIssueServiceImpl.java
+++ project-cluster-service/coin-exchange/coin-admin/admin-service/src/main/java/io/jnyou/service/impl/WorkIssueServiceImpl.java
@@ -1,5 +1,7 @@
 package io.jnyou.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 import java.util.List;
@@ -7,7 +9,29 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import io.jnyou.domain.WorkIssue;
 import io.jnyou.mapper.WorkIssueMapper;
 import io.jnyou.service.WorkIssueService;
+import org.springframework.util.StringUtils;
+
 @Service
 public class WorkIssueServiceImpl extends ServiceImpl<WorkIssueMapper, WorkIssue> implements WorkIssueService{
+
+    /**
+     * 条件分页查询工单列表
+     *
+     * @param page      分页参数
+     * @param status    工单的状态
+     * @param startTime 查询的工单创建起始时间
+     * @param endTime   查询的工单创建截至时间
+     * @return
+     */
+    @Override
+    public Page<WorkIssue> findByPage(Page<WorkIssue> page, Integer status, String startTime, String endTime) {
+        return page(page, new LambdaQueryWrapper<WorkIssue>()
+                .eq(status!=null ,WorkIssue::getStatus,status)
+                .between(
+                        !StringUtils.isEmpty(startTime) && !StringUtils.isEmpty(endTime),
+                        WorkIssue::getCreated,
+                        startTime,endTime+" 23:59:59" )
+        );
+    }
 
 }
