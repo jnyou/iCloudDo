3a0148d25c879b4f78b720f223437dc7fe2a9bb5
jnyou <1447177020@qq.com>
2021-03-08 18:24:49 +0800

    会员银行卡管理

--- project-cluster-service/coin-exchange/coin-member/coin-member-service/src/main/java/io/jnyou/service/UserBankService.java
+++ project-cluster-service/coin-exchange/coin-member/coin-member-service/src/main/java/io/jnyou/service/UserBankService.java
@@ -1,8 +1,18 @@
 package io.jnyou.service;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.jnyou.domain.UserBank;
 import com.baomidou.mybatisplus.extension.service.IService;
 public interface UserBankService extends IService<UserBank>{
 
 
+    /**
+     * 查询用户的银行卡信息
+     * @param page
+     * 分页参数
+     * @param usrId
+     * 用户的Id
+     * @return
+     */
+    Page<UserBank> findByPage(Page<UserBank> page, Long usrId);
 }
--- project-cluster-service/coin-exchange/coin-member/coin-member-service/src/main/java/io/jnyou/service/impl/UserBankServiceImpl.java
+++ project-cluster-service/coin-exchange/coin-member/coin-member-service/src/main/java/io/jnyou/service/impl/UserBankServiceImpl.java
@@ -1,13 +1,26 @@
 package io.jnyou.service.impl;
 
-import org.springframework.stereotype.Service;
-import javax.annotation.Resource;
-import java.util.List;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import io.jnyou.mapper.UserBankMapper;
 import io.jnyou.domain.UserBank;
+import io.jnyou.mapper.UserBankMapper;
 import io.jnyou.service.UserBankService;
+import org.springframework.stereotype.Service;
+
 @Service
-public class UserBankServiceImpl extends ServiceImpl<UserBankMapper, UserBank> implements UserBankService{
+public class UserBankServiceImpl extends ServiceImpl<UserBankMapper, UserBank> implements UserBankService {
+
+    /**
+     * 查询用户的银行卡信息
+     *
+     * @param page  分页参数
+     * @param usrId 用户的Id
+     * @return
+     */
+    @Override
+    public Page<UserBank> findByPage(Page<UserBank> page, Long usrId) {
+        return page(page, new LambdaQueryWrapper<UserBank>().eq(usrId != null, UserBank::getUserId, usrId));
+    }
 
 }
