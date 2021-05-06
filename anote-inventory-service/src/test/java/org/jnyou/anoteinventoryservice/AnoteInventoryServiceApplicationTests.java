63ac29f43c85026fd69865082801a5e5b74deaf3
jnyou <1447177020@qq.com>
2021-05-06 11:33:40 +0800

    新增最小编辑距离算法demo

--- anote-inventory-service/src/test/java/org/jnyou/anoteinventoryservice/AnoteInventoryServiceApplicationTests.java
+++ anote-inventory-service/src/test/java/org/jnyou/anoteinventoryservice/AnoteInventoryServiceApplicationTests.java
@@ -1,13 +1,24 @@
 package org.jnyou.anoteinventoryservice;
 
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.date.DateUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
 
 @SpringBootTest
 class AnoteInventoryServiceApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    public static void main(String[] args) {
+        DateTime date = DateUtil.date();
+        Date uploadTimeBegin = DateUtil.beginOfDay(date);
+        System.out.println(date);
+        System.out.println(uploadTimeBegin);
     }
 
 }
