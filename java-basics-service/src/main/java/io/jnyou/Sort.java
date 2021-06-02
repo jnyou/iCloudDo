d0d52ccc48a6d5b00cc6352f22fcbe84bbb41787
jnyou <1447177020@qq.com>
2021-06-02 20:00:57 +0800

    选择排序算法

--- java-basics-service/src/main/java/io/jnyou/Sort.java
+++ java-basics-service/src/main/java/io/jnyou/Sort.java
@@ -54,33 +54,32 @@ class SelectorSort {
     /**
      * 选择第一个数为最小值||最大值 依次比较记录下标，最后换位
      * 50 2 33 90 57 1
-     * 1 50 33 90 57 2 第一轮
-     * 1 2 50 90 57 33 第二轮
-     * 1 2 33 90 57 50 第三轮
-     * 1 2 33 50 90 57 第四轮
-     * 1 2 33 50 57 90 第五轮
-     *
+     * 1 50 33 90 57 2 第一轮 （j = 1）
+     * 1 2 50 90 57 33 第二轮 （j = 2）
+     * 1 2 33 90 57 50 第三轮 （j = 3）
+     * 1 2 33 50 90 57 第四轮 （j = 4）
+     * 1 2 33 50 57 90 第五轮 （j = 5,包含本身，所以j<nums.length;）
      *
      * @param args
      */
     public static void main(String[] args) {
-        Integer []nums = {50, 2, 33, 90, 57, 1};
-        for(int i = 0; i< nums.length -1; i ++){
+        Integer[] nums = {50, 2, 33, 90, 57, 1};
+        for (int i = 0; i < nums.length - 1; i++) {
             int minIdx = i;
-            for(int j = i +1;j<nums.length;j++){
-                if(nums[minIdx] > nums[j]){
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[minIdx] > nums[j]) {
                     // 换下标
                     minIdx = j;
                 }
             }
             // 判断需要交换 的数是否为自己
-            if(minIdx != i){
+            if (minIdx != i) {
                 nums[minIdx] = nums[minIdx] + nums[i];
                 nums[i] = nums[minIdx] - nums[i];
                 nums[minIdx] = nums[minIdx] - nums[i];
             }
         }
-        for(int k = 0; k < nums.length;k++){
+        for (int k = 0; k < nums.length; k++) {
             System.out.println(nums[k]);
         }
     }
---
+++ temporary-storage-service/src/main/java/org/jnyou/practice/Practice.java
@@ -0,0 +1,21 @@
+package org.jnyou.practice;
+
+import lombok.Data;
+
+/**
+ * 代码千万行，注释第一行
+ * 注释不规范，同事泪两行
+ * <p>
+ * Practice
+ *
+ * @version 1.0.0
+ * @author: JnYou
+ **/
+public class Practice {
+
+    public static void main(String[] args) {
+        // 冒泡排序算法
+
+    }
+
+}
