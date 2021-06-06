8e73276772152a96ed66916324dbab1ce67262dc
jnyou <1447177020@qq.com>
2021-06-06 21:50:04 +0800

    数据结构之链表类型

--- java-basics-service/src/main/java/io/jnyou/Linked.java
+++ java-basics-service/src/main/java/io/jnyou/Linked.java
@@ -14,17 +14,50 @@ import lombok.Data;
  **/
 public class Linked {
 
+    public static void main(String[] args) {
+        NodeManager node = new NodeManager();
+        // 添加节点数据
+        node.add(1);
+        node.add(2);
+        node.add(3);
+        node.add(4);
+        node.add(5);
+
+        // 查询节点所有数据
+        node.list();
+
+    }
 
 }
 
 class NodeManager{
     // 定义根节点
     private Node root;
+    // 增删改查
+    public void add(int data){
+        if(root == null) {
+            root = new Node(data);
+        } else {
+            root.addNode(data);
+        }
+    }
+    // TODO
+    public void update(int oldData,int newData){}
+    public void delete(int data){}
+    public void list(){
+        if(root != null){
+            System.out.print(root.getData() + "-->");
+            root.listAllNode();
+            System.out.println();
+        }
+    }
+    public boolean find(int data){return false;}
+    public void insert(int index,int data){}
 
     @Data
     private class Node{
         // 下一个指针，把当前类型作为自己的属性
-        private Node node;
+        private Node next;
         // 存储的数据
         private int data;
 
@@ -33,21 +66,32 @@ class NodeManager{
         }
         // 添加
         public void addNode(int data){
-
+            if(this.next == null){
+                this.next = new Node(data);
+            } else {
+                this.next.addNode(data);
+            }
         }
+        // 删除
         public void delNode(int data){
 
         }
+        // 查询所有节点
         public void listAllNode(){
-
+            if(this.next != null){
+                System.out.print(this.next.getData() + "-->");
+                this.next.listAllNode();
+            }
         }
+        // 查找某个节点
         public boolean findNode(int data){
             return false;
         }
-        public void uodateNode(int oldData,int newData){
+        // 更新节点
+        public void updateNode(int oldData,int newData){
 
         }
-        // 插入
+        // 插入节点
         public void insertNode(int index,int data){
 
         }
