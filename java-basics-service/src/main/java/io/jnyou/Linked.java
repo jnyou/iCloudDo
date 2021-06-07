f5cca03c9b9d0ceac70818279e7fb8d9667fe0ee
jnyou <1447177020@qq.com>
2021-06-07 21:55:52 +0800

    链表数据结构增删改查

--- java-basics-service/src/main/java/io/jnyou/Linked.java
+++ java-basics-service/src/main/java/io/jnyou/Linked.java
@@ -26,11 +26,28 @@ public class Linked {
         // 查询节点所有数据
         node.list();
 
+        // 更新节点数据
+        node.update(2,10);
+
+        node.list();
+
+        // 删除节点数据
+        node.delete(1);
+        node.list();
+
+        System.out.println(node.find(2));
+        System.out.println(node.find(10));
+
+        // 插入
+        node.insert(3,20);
+        node.list();
+
     }
 
 }
 
 class NodeManager{
+    private int currentNode;
     // 定义根节点
     private Node root;
     // 增删改查
@@ -42,8 +59,22 @@ class NodeManager{
         }
     }
     // TODO
-    public void update(int oldData,int newData){}
-    public void delete(int data){}
+    public void update(int oldData,int newData){
+        if(root == null) return;
+        if(root.getData() == oldData){
+            root.setData(newData);
+        } else {
+            root.updateNode(oldData,newData);
+        }
+    }
+    public void delete(int data){
+        if(root == null) return;
+        if(root.getData() == data){
+            root = root.next;
+        } else {
+            root.delNode(data);
+        }
+    }
     public void list(){
         if(root != null){
             System.out.print(root.getData() + "-->");
@@ -51,8 +82,27 @@ class NodeManager{
             System.out.println();
         }
     }
-    public boolean find(int data){return false;}
-    public void insert(int index,int data){}
+    public boolean find(int data){
+        if(root == null) return false;
+        if(root.getData() == data){
+            return true;
+        } else {
+            return root.findNode(data);
+        }
+    }
+    public void insert(int index,int data){
+        if(index < 0) return;
+        currentNode = 0;
+        // 在根节点前面插入
+        if(currentNode == index) {
+            Node node = new Node(data);
+            // 前插法  // 后插法：root.next = node;
+            node.next = root;
+            root = node;
+        } else {
+            root.insertNode(index,data);
+        }
+    }
 
     @Data
     private class Node{
@@ -74,7 +124,12 @@ class NodeManager{
         }
         // 删除
         public void delNode(int data){
-
+            if(this.next == null) return;
+            if(this.next.data == data){
+                this.next = this.next.next;
+            } else {
+                this.next.delNode(data);
+            }
         }
         // 查询所有节点
         public void listAllNode(){
@@ -85,15 +140,35 @@ class NodeManager{
         }
         // 查找某个节点
         public boolean findNode(int data){
+            if(this.next != null){
+                if(this.next.data == data){
+                    return true;
+                } else {
+                    return this.next.findNode(data);
+                }
+            }
             return false;
         }
         // 更新节点
         public void updateNode(int oldData,int newData){
-
+            if(this.next == null) return;
+            if(this.next.data == oldData){
+                this.next.data = newData;
+            } else {
+                this.next.updateNode(oldData,newData);
+            }
         }
         // 插入节点
         public void insertNode(int index,int data){
-
+            if(this.next == null) return;
+            currentNode++;
+            if(currentNode == index){
+                Node node = new Node(data);
+                node.next = this.next;
+                this.next = node;
+            } else {
+                this.next.insertNode(index,data);
+            }
         }
 
 
